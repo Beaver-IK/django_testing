eb8e9e060419efb5cd0ca07f73b1f3fb6fd9fddb
Ivan Kontsov <ivan.kontsov@gmail.com>
2024-11-09 15:47:39 +0300

    Making edits to ya_news/news/pytest_tests/test_routes.py

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -5,7 +5,7 @@ from pytest_django.asserts import assertRedirects
 from pytest_lazyfixture import lazy_fixture as lf
 
 pytestmark = pytest.mark.django_db
-
+# Адреса страниц
 LOGIN = lf('login_page')
 LOGOUT = lf('logout_page')
 SIGNUP = lf('signup_page')
@@ -13,39 +13,36 @@ HOME = lf('home_page')
 DETAIL = lf('detail_page')
 EDIT = lf('edit_page')
 DELETE = lf('delete_page')
+# Клиенты пользователей
+CLIENT = lf('client')
+AUTHOR_CLIENT = lf('author_client')
+READER_CLIENT = lf('reader_client')
 
 
 @pytest.mark.parametrize(
-    'name', (
-        LOGIN,
-        LOGOUT,
-        SIGNUP,
-        HOME,
-        DETAIL,),)
-def test_pages_availability(client, name):
-    response = client.get(name)
-    assert response.status_code == HTTPStatus.OK
+    'url, client_user,  status', (
+        (LOGIN, CLIENT, HTTPStatus.OK,),
+        (SIGNUP, CLIENT, HTTPStatus.OK,),
+        (LOGOUT, CLIENT, HTTPStatus.OK,),
+        (HOME, CLIENT, HTTPStatus.OK,),
+        (DETAIL, CLIENT, HTTPStatus.OK,),
+        (EDIT, AUTHOR_CLIENT, HTTPStatus.OK,),
+        (DELETE, AUTHOR_CLIENT, HTTPStatus.OK,),
+        (EDIT, READER_CLIENT, HTTPStatus.NOT_FOUND,),
+        (DELETE, READER_CLIENT, HTTPStatus.NOT_FOUND,),),)
+def test_pages_availability(url, client_user, status):
+    response = client_user.get(url)
+    assert response.status_code == status
 
 
-@pytest.mark.parametrize(
-    'client_user, status_user',
-    (
-        (pytest.lazy_fixture('author_client'), HTTPStatus.OK),
-        (pytest.lazy_fixture('reader_client'), HTTPStatus.NOT_FOUND),
-        (pytest.lazy_fixture('client'), HTTPStatus.FOUND),
-    ),
-)
 @pytest.mark.parametrize(
     'name', (
         EDIT,
         DELETE,),)
-def test_availability_for_comment_edit_and_delete(client_user, login_page,
-                                                  name, status_user):
-    """Тест на проверку редактирования и удаления
-    комментария автором, читателем и анонимом.
+def test_availability_for_comment_edit_and_delete(client, login_page, name):
+    """Тест перенаправления незалогинененного пользователя,
+    при попытке редактирования или удаления комментария.
     """
-    response = client_user.get(name)
+    response = client.get(name)
     expected_url = f'{login_page}?next={name}'
-    assert response.status_code == status_user
-    if response.status_code == HTTPStatus.FOUND:
-        assertRedirects(response, expected_url)
+    assertRedirects(response, expected_url)
